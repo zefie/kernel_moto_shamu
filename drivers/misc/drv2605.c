f0b907bb33d11570dabb810e04cec041dd199c76
imoseyon <imoseyon@gmail.com>
2015-01-11 12:55:44 -0800

    drv2605: vibe strength control
    * sysfs set to /sys/vibrator/pwmvalue for compatibility with Trickster
    
    Change-Id: I11183fee917fc9b7563073eec50186867646aee2
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/misc/drv2605.c
+++ drivers/misc/drv2605.c
@@ -163,11 +163,8 @@
 #define I2C_RETRY_DELAY		20 /* ms */
 #define I2C_RETRIES		5
 
-/*
- * Default RTP_STRENGTH is 0x7F which in decimal is 127
- * it's a little to harsh, let's try 100 as the default
- */
-static int rtp_strength = 0x64;
+static struct kobject *vibe_kobj;
+static int vibe_strength;
 
 static struct drv260x {
 	struct class *class;
@@ -593,7 +590,7 @@ static void vibrator_enable(struct timed_output_dev *dev, int value)
 		if (mode != MODE_REAL_TIME_PLAYBACK) {
 			if (audio_haptics_enabled && mode == MODE_AUDIOHAPTIC)
 				setAudioHapticsEnabled(NO);
-			drv260x_set_rtp_val(rtp_strength);
+			drv260x_set_rtp_val(vibe_strength);
 			drv260x_change_mode(MODE_REAL_TIME_PLAYBACK);
 			vibrator_is_playing = YES;
 		}
@@ -1157,45 +1154,31 @@ static struct file_operations fops = {
 	.write = drv260x_write
 };
 
-static ssize_t show_rtp_strength(struct device *dev,
-		struct device_attribute *attr, char *buf)
+static ssize_t pwmvalue_show(struct device *dev,
+                struct device_attribute *attr, char *buf)
 {
-	if (rtp_strength == 0)
-		return snprintf(buf, PAGE_SIZE, "%d\n", 0);
-	else
-		return snprintf(buf, PAGE_SIZE, "%d\n", rtp_strength - 27);
+        size_t count = 0;
+        count += sprintf(buf, "%d\n", vibe_strength);
+        return count;
 }
 
-static ssize_t store_rtp_strength(struct device *dev,
-		struct device_attribute *attr, const char *buf, size_t count)
+static ssize_t pwmvalue_store(struct device *dev,
+                struct device_attribute *attr, const char *buf, size_t count)
 {
-	int ret;
-	unsigned long input;
-
-	ret = kstrtoul(buf, 0, &input);
-	if (ret < 0)
-		return ret;
-
-	if (input > 100)
-		rtp_strength = 127;
-	else if (input <= 0)
-		rtp_strength = 0;
-	else
-		rtp_strength = input + 27;
-
-	return count;
+	int vs = 0;
+        sscanf(buf, "%d ",&vs);
+        if (vs < 0 || vs > 127) vs = 100;
+	vibe_strength = vs;
+        return count;
 }
 
-static DEVICE_ATTR(rtp_strength, (S_IWUSR|S_IRUGO),
-	show_rtp_strength, store_rtp_strength);
-
-struct kobject *drv2605_kobj;
-EXPORT_SYMBOL_GPL(drv2605_kobj);
+static DEVICE_ATTR(pwmvalue, (S_IWUSR|S_IRUGO), pwmvalue_show, pwmvalue_store);
 
 static int drv260x_init(void)
 {
 	int reval = -ENOMEM;
 
+	vibe_strength = REAL_TIME_PLAYBACK_STRENGTH;
 	drv260x = kmalloc(sizeof *drv260x, GFP_KERNEL);
 	if (!drv260x) {
 		printk(KERN_ALERT
@@ -1260,6 +1243,10 @@ static int drv260x_init(void)
 	}
 
 	printk(KERN_ALERT "drv260x: initialized\n");
+
+	vibe_kobj = kobject_create_and_add("vibrator", NULL);
+	if (!vibe_kobj) return 0;
+	reval = sysfs_create_file(vibe_kobj, &dev_attr_pwmvalue.attr);
 	return 0;
 
  fail6:
@@ -1277,7 +1264,7 @@ static int drv260x_init(void)
 
 static void drv260x_exit(void)
 {
-	kobject_del(drv2605_kobj);
+	kobject_del(vibe_kobj);
 	gpio_direction_output(drv260x->en_gpio, GPIO_LEVEL_LOW);
 	gpio_free(drv260x->en_gpio);
 	if (!IS_ERR(drv260x->vibrator_vdd))
