5ddbd417e214c76416a0d7345e3b9f246c94507d
javilonas <admin@lonasdigital.com>
2015-07-27 01:52:47 -0400

    cpufreq: Add new governor Barry-Allen [@Javilonas]
    Signed-off-by: placiano <placiano80@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h
    
    Change-Id: If57f7f85fa890243f186ebcc16c485d20624a7eb

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -451,6 +451,8 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HELLSACTIVE)
 extern struct cpufreq_governor cpufreq_gov_hellsactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_hellsactive)
+extern struct cpufreq_governor cpufreq_gov_barry_allen;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_barry_allen)
 #endif
 
 /*********************************************************************
