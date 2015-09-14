d16839330c4adf9aa18028ba4936e55684c7a0ca
mydongistiny <jaysonedson@gmail.com>
2015-09-14 14:00:09 -0700

    build: Add back in build script and ramdisk files.

---
+++ build.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+# Bash Color
+green='\033[01;32m'
+red='\033[01;31m'
+blink_red='\033[05;31m'
+restore='\033[0m'
+
+clear
+
+# Resources
+THREAD="-j10"
+KERNEL="zImage-dtb"
+DEFCONFIG="benzo_defconfig"
+
+# Kernel Details
+BC="benzoCore"
+VER="b26c"
+BC_VER=$BC-$VER
+
+# Vars
+export USE_CCACHE=1
+export LOCALVERSION=-`echo $VER`
+export ARCH=arm
+export SUBARCH=arm
+export KBUILD_BUILD_USER=xanaxdroid
+export KBUILD_BUILD_HOST=benzo
+CROSS_COMPILE=../arm-eabi-6.0/bin/arm-eabi-
+
+if [ "$USE_CCACHE" = 1 ]; then
+   export CROSS_COMPILE="ccache $CROSS_COMPILE"
+else
+   export CROSS_COMPILE="$CROSS_COMPILE"
+fi
+
+# Paths
+KERNEL_DIR=`pwd`
+REPACK_DIR="$KERNEL_DIR/zip/bCzip"
+ZIP_MOVE="$KERNEL_DIR/zip/"
+ZIMAGE_DIR="$KERNEL_DIR/arch/arm/boot"
+
+# Functions
+function clean_all {
+		rm -rf $REPACK_DIR/kernel/zImage
+		rm -rf $ZIMAGE_DIR/$KERNEL
+		make clean && make mrproper
+}
+
+function make_kernel {
+		make $DEFCONFIG
+		make $THREAD
+		cp -vr $ZIMAGE_DIR/$KERNEL ${KERNEL_DIR}/zip/bCzip/kernel/zImage
+}
+
+function make_zip {
+		cd $REPACK_DIR
+		zip -9 -r `echo $BC_VER`.zip .
+		mv  `echo $BC_VER`.zip $ZIP_MOVE
+		cd $KERNEL_DIR
+}
+
+DATE_START=$(date +"%s")
+
+echo -e "${green}"
+echo "benzoCore Kernel Creation Script:"
+echo
+
+echo "---------------"
+echo "Kernel Version:"
+echo "---------------"
+
+echo -e "${red}"; echo -e "${blink_red}"; echo "$BC_VER"; echo -e "${restore}";
+
+echo -e "${green}"
+echo "-----------------"
+echo "Making benzoCore Kernel:"
+echo "-----------------"
+echo -e "${restore}"
+
+while read -p "Please choose your option: [1]clean-build / [2]dirty-build / [3]abort " cchoice
+do
+case "$cchoice" in
+	1 )
+		echo -e "${green}"
+		echo
+		echo "[..........Cleaning up..........]"
+		echo
+		echo -e "${restore}"
+		clean_all
+		echo -e "${green}"
+		echo
+		echo "[....Building `echo $BC_VER`....]"
+		echo
+		echo -e "${restore}"
+		make_kernel
+		echo -e "${green}"
+		echo
+		echo "[....Make `echo $BC_VER`.zip....]"
+		echo
+		echo -e "${restore}"
+		make_zip
+		echo -e "${green}"
+		echo
+		echo "[.....Moving `echo $BC_VER`.....]"
+		echo -e "${restore}"
+		break
+		;;
+	2 )
+		echo -e "${green}"
+		echo
+		echo "[....Building `echo $BC_VER`....]"
+		echo
+		echo -e "${restore}"
+		make_kernel
+		echo -e "${green}"
+		echo
+		echo "[....Make `echo $BC_VER`.zip....]"
+		echo
+		echo -e "${restore}"
+		make_zip
+		echo -e "${green}"
+		echo
+		echo "[.....Moving `echo $BC_VER`.....]"
+		echo -e "${restore}"
+		break
+		;;
+	3 )
+		break
+		;;
+	* )
+		echo -e "${red}"
+		echo
+		echo "Invalid try again!"
+		echo
+		echo -e "${restore}"
+		;;
+esac
+done
+
+echo -e "${green}"
+echo "-------------------"
+echo "Build Completed in:"
+echo "-------------------"
+echo -e "${restore}"
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo
+
---
+++ zip/bCzip/kernel/edit_ramdisk.sh
@@ -0,0 +1,82 @@
+#!/sbin/sh
+#ramdisk_gov_sed.sh by show-p1984
+#Features:
+#extracts ramdisk
+#finds busbox in /system or sets default location if it cannot be found
+#add init.d support if not already supported
+#removes forced encryption
+#disables mpdecision
+#repacks the ramdisk
+
+mkdir /tmp/ramdisk
+cp /tmp/boot.img-ramdisk.gz /tmp/ramdisk/
+cd /tmp/ramdisk/
+gunzip -c /tmp/ramdisk/boot.img-ramdisk.gz | cpio -i
+cd /
+
+#remove cmdline parameters we do not want
+#maxcpus=2 in this case, which limits smp activation to the first 2 cpus
+echo $(cat /tmp/boot.img-cmdline | sed -e 's/maxcpus=[^ ]\+//')>/tmp/boot.img-cmdline
+
+#Don't force encryption
+if  grep -qr forceencrypt /tmp/ramdisk/fstab.shamu; then
+   sed -i "s/forceencrypt/encryptable/" /tmp/ramdisk/fstab.shamu
+fi
+
+#add init.d support if not already supported
+#this is no longer needed as the ramdisk now inserts our modules, but we will
+#keep this here for user comfort, since having run-parts init.d support is a
+#good idea anyway.
+found=$(find /tmp/ramdisk/init.rc -type f | xargs grep -oh "run-parts /system/etc/init.d");
+if [ "$found" != 'run-parts /system/etc/init.d' ]; then
+        #find busybox in /system
+        bblocation=$(find /system/ -name 'busybox')
+        if [ -n "$bblocation" ] && [ -e "$bblocation" ] ; then
+                echo "BUSYBOX FOUND!";
+                #strip possible leading '.'
+                bblocation=${bblocation#.};
+        else
+                echo "BUSYBOX NOT FOUND! init.d support will not work without busybox!";
+                echo "Setting busybox location to /system/xbin/busybox! (install it and init.d will work)";
+                #set default location since we couldn't find busybox
+                bblocation="/system/xbin/busybox";
+        fi
+	#append the new lines for this option at the bottom
+        echo "" >> /tmp/ramdisk/init.rc
+        echo "service userinit $bblocation run-parts /system/etc/init.d" >> /tmp/ramdisk/init.rc
+        echo "    oneshot" >> /tmp/ramdisk/init.rc
+        echo "    class late_start" >> /tmp/ramdisk/init.rc
+        echo "    user root" >> /tmp/ramdisk/init.rc
+        echo "    group root" >> /tmp/ramdisk/init.rc
+fi
+
+#add benzoCore's init script without replacing all of init.shamu.rc.
+foundbrc=$(find /tmp/ramdisk/init.shamu.rc -type f | xargs grep -oh "import init.benzo.rc");
+if [ "$foundbrc" != 'import init.benzo.rc' ]; then
+	sed -i 's/import init.shamu.diag.rc/import init.benzo.rc'"\n"'import init.shamu.diag.rc/g' /tmp/ramdisk/init.shamu.rc
+fi
+
+#copy fstab
+cp /tmp/fstab.shamu /tmp/ramdisk/fstab.shamu
+chmod 750 /tmp/ramdisk/fstab.shamu
+
+#copy custom init.shamu.power.rc
+cp /tmp/init.shamu.power.rc /tmp/ramdisk/init.shamu.power.rc
+chmod 750 /tmp/ramdisk/init.shamu.power.rc
+
+#copy init.benzo.rc
+cp /tmp/init.benzo.rc /tmp/ramdisk/init.benzo.rc
+chmod 750 /tmp/ramdisk/init.benzo.rc
+
+#copy benzo-post-boot
+mkdir /tmp/ramdisk/sbin
+cp /tmp/benzo-post-boot.sh /tmp/ramdisk/sbin/benzo-post-boot.sh
+chmod 750 /tmp/ramdisk/sbin/benzo-post-boot.sh
+
+rm /tmp/ramdisk/boot.img-ramdisk.gz
+rm /tmp/boot.img-ramdisk.gz
+cd /tmp/ramdisk/
+find . | cpio -o -H newc | gzip > ../boot.img-ramdisk.gz
+cd /
+rm -rf /tmp/ramdisk
+
